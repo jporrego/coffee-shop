680869670fe698633e46c5e1be5d9f8a6fafcba2
Juan Pablo Orrego Contador <jporregoc@gmail.com>
2022-05-19 21:40:30 -0400

    Filtering working. Test needed.

--- src/components/filter-container/FilterContainer.tsx
+++ src/components/filter-container/FilterContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Product } from "../../types";
 import Filter from "../filter/Filter";
 import "./FilterContainer.css";
@@ -16,6 +16,10 @@ const FilterContainer: React.FC<FilterContainerProps> = ({
 }) => {
   const [filters, setFilters] = useState<object[]>([]);
 
+  useEffect(() => {
+    filterProducts2();
+  }, [filters]);
+
   const filterProducts = (e: React.ChangeEvent<HTMLSelectElement>) => {
     if (e.target.value === "all") {
       setFilteredProducts(products);
@@ -26,6 +30,38 @@ const FilterContainer: React.FC<FilterContainerProps> = ({
           return product;
         }
       });
+      setFilteredProducts(filteredArray);
+    }
+  };
+
+  const filterProducts2 = () => {
+    let filteredArray: Product[] = [];
+
+    /*for (const filter of filters) {
+      for (const product of products) {
+        if (
+          // @ts-ignore
+          product[Object.keys(filter)[0]] === filter[Object.keys(filter)[0]]
+        ) {
+          filteredArray.push(product);
+        }
+      }
+    }*/
+
+    for (const product of products) {
+      let matches = true;
+      for (const filter of filters) {
+        if (
+          // @ts-ignore
+          product[Object.keys(filter)[0]] !== filter[Object.keys(filter)[0]]
+        ) {
+          matches = false;
+          break;
+        }
+      }
+      if (matches) {
+        filteredArray.push(product);
+      }
       setFilteredProducts(filteredArray);
     }
   };
