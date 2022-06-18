f1aa8ed3a0f5d2b08279150721471f7700eb6f47
Juan Pablo Orrego Contador <jporregoc@gmail.com>
2022-06-18 16:56:17 -0400

    Added loading gif while fetching the server

--- src/components/product-list/ProductList.tsx
+++ src/components/product-list/ProductList.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./ProductList.css";
 import { Product as ProductInterface } from "../../types";
 import Product from "../product/Product";
+import Spinner from "../../assets/spinner.svg";
 
 interface ProductListProps {
   products: ProductInterface[];
@@ -15,22 +16,56 @@ const ProductList: React.FC<ProductListProps> = ({
   onAddToCart,
   openProductModal,
 }) => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [errorMsg, setErrorMsg] = useState<string>("");
+
   useEffect(() => {
     getProducts();
   }, []);
 
   const getProducts = async () => {
     try {
+      setLoading(true);
       const response = await fetch("http://localhost:4000/");
       const data = await response.json();
       setProducts(data);
+      setLoading(false);
+      setErrorMsg("");
     } catch (error) {
       console.log(error);
+      setErrorMsg("Failed to connect to the server... trying again...");
+      setLoading(false);
     }
+    /*
+    let tryToFetch = true;
+    setTimeout(() => {
+      tryToFetch = false;
+    }, 10000);
+    while (tryToFetch) {
+      try {
+        setLoading(true);
+        const response = await fetch("http://localhost:4000/");
+        const data = await response.json();
+        setProducts(data);
+        setLoading(false);
+        setErrorMsg("");
+        break;
+      } catch (error) {
+        console.log(error);
+        setErrorMsg("Failed to connect to the server... trying again...");
+        setLoading(false);
+      }
+    }*/
   };
 
   return (
     <div className="product-list">
+      {loading && (
+        <div className="spinner">
+          <img src={Spinner} alt="React Logo" />
+        </div>
+      )}
+      {errorMsg}
       {products.map((product) => (
         <Product
           key={product._id}
