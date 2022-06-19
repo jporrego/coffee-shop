06db8fc067c99f506a88e75beefec0de55a04582
Juan Pablo Orrego Contador <jporregoc@gmail.com>
2022-06-19 16:18:57 -0400

    added low res version of bg

--- src/components/product-list/ProductList.tsx
+++ src/components/product-list/ProductList.tsx
@@ -42,24 +42,29 @@ const ProductList: React.FC<ProductListProps> = ({
     let tryToFetch = true;
     setTimeout(() => {
       tryToFetch = false;
-    }, 10000);
+    }, 20000);
     while (tryToFetch) {
-      try {
-        setLoading(true);
-        const response = await fetch("http://localhost:4000/");
-        const data = await response.json();
-        setProducts(data);
-        setLoading(false);
-        setErrorMsg("");
-        break;
-      } catch (error) {
-        console.log(error);
-        setLoading(false);
-        if (tryToFetch) {
-          setErrorMsg("Failed to connect to the server... trying again...");
-        } else {
-          setErrorMsg("Failed to connect to the server.");
+      if (process.env.REACT_APP_API_URL !== undefined) {
+        try {
+          setLoading(true);
+          const response = await fetch(process.env.REACT_APP_API_URL);
+          const data = await response.json();
+          setProducts(data);
+          setLoading(false);
+          setErrorMsg("");
+          break;
+        } catch (error) {
+          console.log(error);
+          setLoading(false);
+          if (tryToFetch) {
+            setErrorMsg("Failed to connect to the server... trying again...");
+          } else {
+            setErrorMsg("Failed to connect to the server.");
+          }
         }
+      } else {
+        setErrorMsg("Failed to connect to the server.");
+        break;
       }
     }
   };
